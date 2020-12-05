521be5ee541b358a0ebc697b2b87cea7579da42c
Rohit Saini <sketchlearn7@gmail.com>
2020-12-05 13:23:17 +0530

    Initial Commit

---
+++ Shared/SwiftUIViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewsApp.swift
+//  Shared
+//
+//  Created by Rohit Saini on 05/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
